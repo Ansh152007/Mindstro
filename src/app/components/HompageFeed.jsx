5ba0bdf983f7d62be76cba266b3b4848b7bcfcc9
Ansh Tiwari <167138279+Ansh152007@users.noreply.github.com>
2025-04-23 00:29:43 +0530

    update in components

--- src/app/components/HompageFeed.jsx
+++ src/app/components/HompageFeed.jsx
@@ -5,18 +5,36 @@ const HompageFeed = () => {
     <div className="relative flex flex-col items-center justify-center w-screen ">
       <ul className="flex flex-col items-center justify-center w-screen">
         <li className="flex flex-col items-center justify-center overflow-y-auto">
-          <div className="w-screen h-48 bg-gray-500">
+          <div className="w-screen flex justify-between items-center">
+            <div className="flex justify-center items-center gap-1 ml-1">
+              <img
+                className="w-10 rounded-full"
+                src="Mindstro_logo.png"
+                alt=""
+              />
+              <div>
+                <p className="font-bold leading-3">Mindstro</p>
+                <p className="text-xs">12h ago</p>
+              </div>
+            </div>
+            <div>
+              <img className="w-7 mr-1" src="morepost.svg" alt="" />
+            </div>
+          </div>
+          <div className="w-screen h-48">
             <img className="w-screen" src="Promotion_image.png" alt="" />
           </div>
-          <h3>Introducing Mindstro: Where Time Meets Value</h3>
-          <p>Earn while you learn. Limit while you chill...</p>
-          <div className="flex justify-between items-center w-screen">
-            <div className="flex gap-2 ml-2">
-              <img className="w0" src="favorite.svg" alt="" />
-              <img src="comment.svg" alt="" />
-              <img src="forward.svg" alt="" />
+          <div className="mt-4">
+            <h3 className="font-bold ml-2">Introducing Mindstro: Where Time Meets value</h3>
+            <p className="ml-2">Earn while you learn. Limit while you chill...</p>
+            <div className="flex justify-between items-center w-screen">
+              <div className="flex gap-2 ml-2 mt-1">
+                <img className="w-6" src="favorite.svg" alt="" />
+                <img className="w-6" src="comment.svg" alt="" />
+                <img className="w-6" src="forward.svg" alt="" />
+              </div>
+              <img className="mr-2" src="save.svg" alt="" />
             </div>
-            <img className="mr-2" src="save.svg" alt="" />
           </div>
         </li>
       </ul>
